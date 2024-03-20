aa487827e058f73844f5b8e832294cfb9ccbd75e
kevin kirui <kipkoechkevin42@gmail.com>
2024-03-20 12:41:22 -0700

    added changes to fizzbuzz

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -8,19 +8,20 @@ def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
+    - For multiples of both three and five, print "FizzBuzz".
+    - For multiples of three, print "Fizz".
+    - For multiples of five, print "Buzz".
+    - For other numbers, print the number itself.
     """
     if n < 1:
         return
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
